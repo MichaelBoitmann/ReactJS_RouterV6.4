92fc6e5e37b9c5736149c628e98ebef8a745da16
Michael <michael.boitmann@gmail.com>
2024-06-09 23:10:16 +1200

    added about page

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='home'>
+    <h2>Welcome</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum optio sit magnam mollitia voluptate nemo dignissimos voluptatibus sequi, quae sint quidem esse quisquam minima in iusto dolorem, doloremque asperiores itaque.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, cupiditate accusamus? Exercitationem, odio cumque eaque porro quibusdam officiis ab optio hic, eligendi quia, distinctio repellendus nesciunt fugit blanditiis. Quisquam, laudantium!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, officiis. Delectus est aperiam inventore autem itaque a culpa ipsam eos, perspiciatis, cum aspernatur laboriosam aliquam voluptate consequatur dolores dolore esse.</p>
+  </div>
+  )
+}
+
+export default About

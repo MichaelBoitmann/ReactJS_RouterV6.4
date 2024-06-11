48f343a19269d380220490ca818b7755c3df6eee
Michael <michael.boitmann@gmail.com>
2024-06-11 22:17:49 +1200

    run json-server to fetch the local api, added careers folder where loader will fetch the json files for careers list

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Faq from './pages/help/Faq'
 import Contact from './pages/help/Contact'
+import Careers, { careersLoader } from './pages/careers/Careers'
 
 import RootLayout from './layouts/RootLayout'
 import HelpLayout from './layouts/HelpLayout'
@@ -27,7 +28,11 @@ const router = createBrowserRouter(
       </Route>
 
       <Route path="careers" element={<CareersLayout />}>
-        <Route index element={<CareersLayout />} />
+        <Route 
+          index 
+          element={<Careers />} 
+          loader={careersLoader}
+        />
       </Route>
 
       <Route path="*" element={<NotFound />} />
